90cf47de37079e45d49ab0268db15c596761b063
Daniel Carvalho <Scar11969@hotmail.com>
2019-07-20 23:58:04 +0100

    Initial setup

---
+++ src/main/java/com/localhost/KYPApplication.java
@@ -0,0 +1,13 @@
+package com.localhost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KYPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KYPApplication.class, args);
+    }
+
+}
